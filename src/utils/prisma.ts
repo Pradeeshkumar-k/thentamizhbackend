c3214d45e81a07b3dc39338c749c2eef3a3b5493
Pradeeshkumar-k <pradeeshkumark600@gmail.com>
2026-02-04 00:52:57 +0530

    fix

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -8,7 +8,7 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     datasources: {
-      db: { url: process.env.DATABASE_URL },
+      db: { url: process.env.DIRECT_URL },
     },
   });
 
