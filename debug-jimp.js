4cdd7c3f073a07fe50f9f331799cfaeb599e0000
Pradeeshkumar-k <pradeeshkumark600@gmail.com>
2026-02-10 17:13:45 +0530

    review

--- debug-jimp.js
+++
@@ -1,5 +0,0 @@
-const Jimp = require('jimp');
-console.log('Type of Jimp:', typeof Jimp);
-console.log('Keys of Jimp:', Object.keys(Jimp));
-console.log('Has read?', typeof Jimp.read);
-console.log('Has Jimp.Jimp?', !!Jimp.Jimp);
--- test-jimp-3.js
+++
@@ -1,7 +0,0 @@
-
-const lib = require('jimp');
-const JimpClass = lib.Jimp;
-
-console.log('lib.read exists?', !!lib.read);
-console.log('JimpClass.read exists?', !!JimpClass.read);
-console.log('diff between lib.Jimp and lib?', lib.Jimp === lib);
